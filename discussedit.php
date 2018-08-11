61d6306fd9e67f2c8c1a7c9c8be356c3d4875cb5
Ryan Lee <gxlhybh@gmail.com>
2018-08-12 02:54:10 +0800

    Add new php file

---
+++ discussedit.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+    <title><?php echo $OJ_NAME ?></title>
+    <?php include("template/$OJ_TEMPLATE/css.php"); ?>
+    <?php include("template/$OJ_TEMPLATE/extra_css.php") ?>
+    <?php include("template/$OJ_TEMPLATE/js.php"); ?>
+    <?php include("template/$OJ_TEMPLATE/extra_js.php") ?>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="http://cdn.bootcss.com/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="http://cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+<?php include("template/$OJ_TEMPLATE/nav.php");
+include("csrf.php");
+?>
+<div class="container">
+    <!-- Main component for a primary marketing message or call to action -->
+    <div class="ui container padding">
+        <div class="ui grid">
+            <div class="row" v-if="!reply">
+                <h2 class="ui header">
+                    Title
+                </h2>
+            </div>
+            <div class="row" v-if="!reply">
+                <div class="ui input">
+                  <input type="text" v-model="title" size="50">
+                </div>
+            </div>
+            <div class="row">
+                <h2 class="ui header">
+                    Content
+                </h2>
+                </div>
+                <div class="row">
+                <mavon-editor v-model="content"></mavon-editor>
+                </div>
+                <div class="row">
+                    <div class="two field">
+    <div class="ui left input" style="width:auto">
+                        <input v-model="captcha" type="text" placeholder="验证码"><img alt="click to change" src="/api/captcha?from=edit" onclick="this.src='/api/captcha?from=edit&random='+Math.random()" height="40px">
+                    </div>
+    </div>
+                </div>
+                <div class="row">
+                    <div class="ui blue labeled submit icon button" @click="edit_post">
+      <i class="icon edit"></i> Modify
+                </div>
+                </div>
+            </div>
+    </div>
+</div> <!-- /container -->
+<script>
+    new Vue({
+                el:".ui.container.padding",
+                data:function(){
+                    return {
+                        content:"",
+                        title:"",
+                        captcha:"",
+                        reply:Boolean(getParameterByName("comment_id")),
+                        article_id:getParameterByName("id"),
+                        comment_id:getParameterByName("comment_id")
+                    }
+                },
+                mounted:function() {
+                    var isMainContent = !this.reply;
+                    var that = this;
+                    if(isMainContent) {
+                        $.get("../api/discuss/update/main/"+this.article_id,function(data){
+                            that.content = data.data.content;
+                            that.title = data.data.title;
+                        })
+                    }
+                    else {
+                        $.get("../api/discuss/update/reply/"+this.article_id+"/"+this.comment_id,function(data){
+                            that.content = data.data.content;
+                        })
+                    }
+                },
+                methods:{
+                    edit_post:function() {
+                        var send = {
+                            title:this.title,
+                            content:this.content,
+                            captcha:this.captcha
+                        }
+                        var isMainContent = !this.reply;
+                        var that = this;
+                        if(isMainContent) {
+                            $.post("../api/discuss/update/main/"+this.article_id,send,function(data){
+                                if(data.status == "OK") {
+                                    alert("更改成功");
+                                    location.href = "/discusscontext.php?id="+that.article_id
+                                }
+                                else {
+                                    alert("Error!\n"+data.statement);
+                                }
+                            })
+                        }
+                        else {
+                            $.post("../api/discuss/update/reply/"+this.article_id+"/"+this.comment_id,send,function(data){
+                               if(data.status == "OK") {
+                                    alert("更改成功");
+                                    location.href = "/discusscontext.php?id="+that.article_id
+                                }
+                                else {
+                                    alert("Error!\n"+data.statement);
+                                }
+                            })
+                        }
+                    }
+                }
+    });
+</script>
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+
+    <?php include("template/$OJ_TEMPLATE/bottom.php") ?>
+</body>
+</html>
---
+++ need_contest_password.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Standard Meta -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <?php include("template/$OJ_TEMPLATE/css.php");?>
+    <!-- Site Properties -->
+    <title>Private Contest -- CUP Online Judge</title>
+    <?php include("template/$OJ_TEMPLATE/js.php");?>
+</head>
+<body>
+<?php include("template/$OJ_TEMPLATE/nav.php"); ?>
+<div class="ui container">
+    <h2 class="ui dividing header">私有竞赛/作业</h2>
+    <form id='contest_form' method='post' class="ui form">
+        <div class="fields">
+            <div class="six wide field">
+                <label>请输入密码</label>
+                <input id='contest_pass' class=input-mini type=password name=password>
+            </div>
+        </div>
+        <input class='ui primary button' type=submit>
+        </form>
+    <script>$('#contest_form').submit(function() {
+        $.post('../api/contest/password/$cid',{
+            password:$('#contest_pass').val()
+        });
+    return true; // return false to cancel form action
+});</script>
+</div>
+<script>
+    $('.ui.container.padding').css('min-height', screen.height);
+</script>
+    <?php include("template/$OJ_TEMPLATE/bottom.php");?>
+</body>
+</html>
