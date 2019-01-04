63875066d012f772b224e1d19378832a87e53cb8
Alexander Xia <xfl03@hotmail.com>
2019-01-04 22:59:52 +0800

    Fix incorrect href in nav.php

--- nav.php
+++ nav.php
@@ -22,16 +22,16 @@ $suffix="?my";
                 <?php } ?>
                 <a href="/" class="item"><i class="home icon"></i>首页</a>
                 <a class="item"
-                   href="problemset.php"><i class="browser icon"></i><?= $MSG_PROBLEM ?></a>
+                   href="/problemset.php"><i class="browser icon"></i><?= $MSG_PROBLEM ?></a>
                 <a class="item"
-                   href="status.php"><i class="tasks icon"></i><?= $MSG_STATUS ?></a>
+                   href="/status.php"><i class="tasks icon"></i><?= $MSG_STATUS ?></a>
                 <a class="item"
-                   href="ranklist.php"><i class="trophy icon"></i><?= $MSG_RANKLIST ?></a>
+                   href="/ranklist.php"><i class="trophy icon"></i><?= $MSG_RANKLIST ?></a>
                    <?php if(isset($_SESSION["user_id"])) { ?>
-                   <a class="item" href="discuss.php"><i class="comment alternate icon"></i>讨论</a>
+                   <a class="item" href="/discuss.php"><i class="comment alternate icon"></i>讨论</a>
                    <?php } ?>
                    <a class="item"
-                           href="contest.php<?=$suffix?>"><i class="puzzle icon"></i><?= $MSG_CONTEST ?></a>
+                           href="/contest.php<?=$suffix?>"><i class="puzzle icon"></i><?= $MSG_CONTEST ?></a>
                 <div
                     class="<?php if (!(isset($_GET['cid']) || isset($_GET['tid']) || $vjudge || $acmmanager)) echo 'ui dropdown' ?> item"
                     tabindex="0">
@@ -43,18 +43,18 @@ $suffix="?my";
                         <i class="dropdown icon"></i>
                     <?php } ?>
                     <div class="menu">
-                        <a class="item" href="faqs.php"><i class="help icon"></i><?= $MSG_FAQ ?></a>
+                        <a class="item" href="/faqs.php"><i class="help icon"></i><?= $MSG_FAQ ?></a>
                         <a class="item"
-                           href="specialsubject.php"><i class="rocket icon"></i><?= $MSG_SPECIALSUBJECT ?></a>
+                           href="/specialsubject.php"><i class="rocket icon"></i><?= $MSG_SPECIALSUBJECT ?></a>
                         <a class="item"
-                           href="recent-contest.php"><i class="sitemap icon"></i><?= $MSG_RECENT_CONTEST ?></a>
-                           <a class="item" href="acmmanager.php"><i class="road icon"></i>ACM管理系统</a>
-                           <a class="item" href="software.php"><i class="plug icon"></i>常用软件</a>
-                           <a class="item" href="whiteboard.php"><i class="calendar outline icon"></i>白板(β版测试)</a>
+                           href="/recent-contest.php"><i class="sitemap icon"></i><?= $MSG_RECENT_CONTEST ?></a>
+                           <a class="item" href="/acmmanager.php"><i class="road icon"></i>ACM管理系统</a>
+                           <a class="item" href="/software.php"><i class="plug icon"></i>常用软件</a>
+                           <a class="item" href="/whiteboard.php"><i class="calendar outline icon"></i>白板(β版测试)</a>
                            <?php if(!$OJ_CONTEST_MODE||isset($_SESSION['administrator'])){ ?>
                 <a class="item tutorial" target="_blank"><i class="question circle outline icon"></i>C/C++语言参考手册</a>
                                 <a class="item" href="https://wiki.cupacm.com"><i class="book icon"></i>Wiki</a>
-                                <a class="item" href="fame.php"><i class="chess queen icon"></i>Hall of Fame</a>
+                                <a class="item" href="/fame.php"><i class="chess queen icon"></i>Hall of Fame</a>
 
                 <?php } ?>
                            
@@ -98,7 +98,7 @@ $suffix="?my";
             <a class="item" href="<?php echo $path_fix ?>vjudgeproblemset.php"><i class="browser icon"></i>
                 <?php echo $MSG_PROBLEMS ?>
             </a>
-            <a class="item" href="hdu_status.php"><i class="tasks icon"></i>
+            <a class="item" href="/hdu_status.php"><i class="tasks icon"></i>
                 <?php echo $MSG_STATUS ?>
             </a>
             <a class="item"
@@ -115,10 +115,10 @@ $suffix="?my";
                 </div>
             <?php } else { ?>
                 <div class="item">
-                    <a class="ui button" href="newloginpage.php"><?= $MSG_LOGIN ?></a>
+                    <a class="ui button" href="/newloginpage.php"><?= $MSG_LOGIN ?></a>
                 </div>
                 <div class="item">
-                    <a class="ui primary button" href="registerpage.php"><?= $MSG_REGISTER ?></a>
+                    <a class="ui primary button" href="/registerpage.php"><?= $MSG_REGISTER ?></a>
                 </div>
             <?php } ?>
         </div>
@@ -148,13 +148,13 @@ $suffix="?my";
                 <?php } ?>
                 
                 <a class="item"
-                   href="problemset.php"><?= $MSG_PROBLEM ?></a>
+                   href="/problemset.php"><?= $MSG_PROBLEM ?></a>
                 <a class="item"
-                   href="status.php"><?= $MSG_STATUS ?></a>
+                   href="/status.php"><?= $MSG_STATUS ?></a>
                 <a class="item"
                    href="<?php echo $path_fix ?>ranklist.php"><?= $MSG_RANKLIST ?></a>
                    <a class="item"
-                           href="contest.php<?=$suffix?>"><?= $MSG_CONTEST ?></a>
+                           href="/contest.php<?=$suffix?>"><?= $MSG_CONTEST ?></a>
                 <div
                     class="<?php if (!(isset($_GET['cid']) || isset($_GET['tid']) || $vjudge || $acmmanager)) echo 'ui dropdown' ?> item"
                     tabindex="0">
@@ -166,20 +166,20 @@ $suffix="?my";
                         <i class="dropdown icon"></i>
                     <?php } ?>
                     <div class="menu">
-                        <a class="item" href="faqs.php"><?= $MSG_FAQ ?></a>
+                        <a class="item" href="/faqs.php"><?= $MSG_FAQ ?></a>
                         <a class="item"
-                           href="specialsubject.php"><?= $MSG_SPECIALSUBJECT ?></a>
+                           href="/specialsubject.php"><?= $MSG_SPECIALSUBJECT ?></a>
                         <a class="item"
-                           href="recent-contest.php"><?= $MSG_RECENT_CONTEST ?></a>
-                           <a class="item" href="acmmanager.php">ACM管理系统</a>
-                           <a class="item" href="software.php">常用软件</a>
-                           <a class="item" href="whiteboard.php">白板(β版测试)</a>
+                           href="/recent-contest.php"><?= $MSG_RECENT_CONTEST ?></a>
+                           <a class="item" href="/acmmanager.php">ACM管理系统</a>
+                           <a class="item" href="/software.php">常用软件</a>
+                           <a class="item" href="/whiteboard.php">白板(β版测试)</a>
                             <a class="item" href="/vjudgeproblemset.php">Virtual
                             Judge</a>
                 
                 <a class="item" href="https://wiki.cupacm.com">Wiki</a>
                 <?php if(!$OJ_CONTEST_MODE||isset($_SESSION['administrator'])){ ?>
-                <a class="item tutorial" href="cppreference/en/" target="_blank">C/C++语言参考手册</a>
+                <a class="item tutorial" href="/cppreference/en/" target="_blank">C/C++语言参考手册</a>
                 <?php } ?>
                     </div>
                 </div>
@@ -217,7 +217,7 @@ $suffix="?my";
             <a class="item" href="<?php echo $path_fix ?>vjudgeproblemset.php">
                 <?php echo $MSG_PROBLEMS ?>
             </a>
-            <a class="item" href="hdu_status.php">
+            <a class="item" href="/hdu_status.php">
                 <?php echo $MSG_STATUS ?>
             </a>
             <a class="item"
@@ -234,10 +234,10 @@ $suffix="?my";
                 </div>
             <?php } else { ?>
                 <div class="item">
-                    <a class="ui button" href="newloginpage.php"><?= $MSG_LOGIN ?></a>
+                    <a class="ui button" href="/newloginpage.php"><?= $MSG_LOGIN ?></a>
                 </div>
                 <div class="item">
-                    <a class="ui primary button" href="registerpage.php"><?= $MSG_REGISTER ?></a>
+                    <a class="ui primary button" href="/registerpage.php"><?= $MSG_REGISTER ?></a>
                 </div>
             <?php } ?>
         </div>
@@ -263,11 +263,11 @@ $suffix="?my";
             <div class="text">通用</div>
             <i class="dropdown icon"></i>
             <div class="menu">
-                <a class="item" href="faqs.php"><?= $MSG_FAQ ?></a>
+                <a class="item" href="/faqs.php"><?= $MSG_FAQ ?></a>
                 <a class="item"
-                   href="problemset.php"><?= $MSG_PROBLEM ?></a>
+                   href="/problemset.php"><?= $MSG_PROBLEM ?></a>
                 <a class="item"
-                   href="status.php"><?= $MSG_STATUS ?></a>
+                   href="/status.php"><?= $MSG_STATUS ?></a>
                 <a class="item"
                    href="<?php echo $path_fix ?>ranklist.php"><?= $MSG_RANKLIST ?></a>
                 <div class="item" tabindex="0">
@@ -275,20 +275,20 @@ $suffix="?my";
                     <div class="text">其他</div>
                     <div class="menu">
                         <a class="item"
-                           href="contest.php<?=$suffix?>"><?= $MSG_CONTEST ?></a>
+                           href="/contest.php<?=$suffix?>"><?= $MSG_CONTEST ?></a>
                         <a class="item"
-                           href="specialsubject.php"><?= $MSG_SPECIALSUBJECT ?></a>
+                           href="/specialsubject.php"><?= $MSG_SPECIALSUBJECT ?></a>
                         <a class="item"
-                           href="recent-contest.php"><?= $MSG_RECENT_CONTEST ?></a>
-                           <a class="item" href="acmmanager.php">ACM管理系统</a>
-                           <a class="item" href="software.php">常用软件</a>
-                           <a class="item" href="whiteboard.php">白板(β版测试)</a>
+                           href="/recent-contest.php"><?= $MSG_RECENT_CONTEST ?></a>
+                           <a class="item" href="/acmmanager.php">ACM管理系统</a>
+                           <a class="item" href="/software.php">常用软件</a>
+                           <a class="item" href="/whiteboard.php">白板(β版测试)</a>
                             <a class="item" href="/vjudgeindex.php">Virtual
                             Judge</a>
                 
                 <a class="item" href="https://wiki.cupacm.com">Wiki</a>
                 <?php if(!$OJ_CONTEST_MODE||isset($_SESSION['administrator'])){ ?>
-                <a class="item tutorial" href="cppreference/en/" target="_blank">C/C++语言参考手册</a>
+                <a class="item tutorial" href="/cppreference/en/" target="_blank">C/C++语言参考手册</a>
                 <?php } ?>
                     </div>
                 </div>
@@ -325,7 +325,7 @@ $suffix="?my";
             <a class="item" href="<?php echo $path_fix ?>vjudgeproblemset.php">
                 <?php echo $MSG_PROBLEMS ?>
             </a>
-            <a class="item" href="hdu_status.php">
+            <a class="item" href="/hdu_status.php">
                 <?php echo $MSG_STATUS ?>
             </a>
             <a class="item"
@@ -342,10 +342,10 @@ $suffix="?my";
                 </div>
             <?php } else { ?>
                 <div class="item">
-                    <a class="ui button" href="newloginpage.php"><?= $MSG_LOGIN ?></a>
+                    <a class="ui button" href="/newloginpage.php"><?= $MSG_LOGIN ?></a>
                 </div>
                 <div class="item">
-                    <a class="ui primary button" href="registerpage.php"><?= $MSG_REGISTER ?></a>
+                    <a class="ui primary button" href="/registerpage.php"><?= $MSG_REGISTER ?></a>
                 </div>
             <?php } ?>
         </div>
@@ -405,8 +405,8 @@ if(location.pathname.substring(1).length > 0 && !location.pathname.match(/index/
     $(".hidemenu").show();
 }
   </script>
-<script src="template/semantic-ui/js/cookie.js"></script>
-<script src="template/semantic-ui/js/platform.js?ver=1.0.1"></script>
+<script src="/template/semantic-ui/js/cookie.js"></script>
+<script src="/template/semantic-ui/js/platform.js?ver=1.0.1"></script>
 <?php 
 require_once("template/semantic-ui/websocket.php");
 ?>
